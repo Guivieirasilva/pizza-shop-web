04fe630a0c7c194611ff9264a337f2aac2a915a3
gui vieira <guivieirasilva02@gmail.com>
2024-06-04 20:13:50 -0300

    feat: retorno das metricas do dashboard

--- src/pages/app/dashboard/day-orders-amount-card.tsx
+++ src/pages/app/dashboard/day-orders-amount-card.tsx
@@ -1,8 +1,15 @@
+import { useQuery } from '@tanstack/react-query'
 import { Utensils } from 'lucide-react'
 
+import { getDaysOrdersAmount } from '@/api/get-day-orders-amount'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export function DayOrdersAmountCard() {
+  const { data: daysOrdersAmount } = useQuery({
+    queryFn: getDaysOrdersAmount,
+    queryKey: ['metrics', 'day-orders-amount'],
+  })
+
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2 ">
@@ -12,11 +19,31 @@ export function DayOrdersAmountCard() {
         <Utensils className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">250</span>
-        <p className="text-sm text-muted-foreground">
-          <span className="text-rose-500 dark:text-rose-400"> -4% </span>
-          em relação a ontem
-        </p>
+        {daysOrdersAmount && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {daysOrdersAmount.amount.toLocaleString('pt-BR')}
+            </span>
+            <p className="text-sm text-muted-foreground">
+              {daysOrdersAmount.diffFromYesterday >= 0 ? (
+                <>
+                  <span className="text-emerald-500 dark:text-emerald-400">
+                    +{daysOrdersAmount.diffFromYesterday}%{' '}
+                  </span>
+                  em relação a ontem
+                </>
+              ) : (
+                <>
+                  <span className="text-rose-500 dark:text-rose-400">
+                    {' '}
+                    -{daysOrdersAmount.diffFromYesterday}%{' '}
+                  </span>
+                  em relação a ontem
+                </>
+              )}
+            </p>
+          </>
+        )}
       </CardContent>
     </Card>
   )
--- src/pages/app/dashboard/popular-products-charts.tsx
+++ src/pages/app/dashboard/popular-products-charts.tsx
@@ -1,16 +1,10 @@
+import { useQuery } from '@tanstack/react-query'
 import { BarChart } from 'lucide-react'
 import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
 import colors from 'tailwindcss/colors'
 
+import { getPopularProducts } from '@/api/get-popular-products'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-
-const data = [
-  { product: 'Pepperoni', amount: 10 },
-  { product: 'Mussarela', amount: 15 },
-  { product: 'Calabresa', amount: 20 },
-  { product: 'Frango c/ Catupiry', amount: 15 },
-  { product: 'Marguerita', amount: 10 },
-]
 
 const COLORS = [
   colors.sky['500'],
@@ -21,6 +15,11 @@ const COLORS = [
 ]
 
 export function PopularProductsChart() {
+  const { data: popuularProdutcs } = useQuery({
+    queryFn: getPopularProducts,
+    queryKey: ['metrics', 'popular-products'],
+  })
+
   return (
     <Card className="col-span-3">
       <CardHeader className="pb-8">
@@ -32,61 +31,65 @@ export function PopularProductsChart() {
         </div>
       </CardHeader>
       <CardContent>
-        <ResponsiveContainer width="100%" height={240}>
-          <PieChart style={{ fontSize: 12 }}>
-            <Pie
-              data={data}
-              dataKey="amount"
-              nameKey="product"
-              cx="50%"
-              cy="50%"
-              outerRadius={86}
-              innerRadius={64}
-              strokeWidth={8}
-              fill={colors.emerald['500']}
-              labelLine={false}
-              label={({
-                cx,
-                cy,
-                midAngle,
-                innerRadius,
-                outerRadius,
-                value,
-                index,
-              }) => {
-                const RADIAN = Math.PI / 180
-                const radius = 12 + innerRadius + (outerRadius - innerRadius)
-                const x = cx + radius * Math.cos(-midAngle * RADIAN)
-                const y = cy + radius * Math.sin(-midAngle * RADIAN)
+        {popuularProdutcs && (
+          <ResponsiveContainer width="100%" height={240}>
+            <PieChart style={{ fontSize: 12 }}>
+              <Pie
+                data={popuularProdutcs}
+                dataKey="amount"
+                nameKey="product"
+                cx="50%"
+                cy="50%"
+                outerRadius={86}
+                innerRadius={64}
+                strokeWidth={8}
+                fill={colors.emerald['500']}
+                labelLine={false}
+                label={({
+                  cx,
+                  cy,
+                  midAngle,
+                  innerRadius,
+                  outerRadius,
+                  value,
+                  index,
+                }) => {
+                  const RADIAN = Math.PI / 180
+                  const radius = 12 + innerRadius + (outerRadius - innerRadius)
+                  const x = cx + radius * Math.cos(-midAngle * RADIAN)
+                  const y = cy + radius * Math.sin(-midAngle * RADIAN)
 
-                return (
-                  <text
-                    x={x}
-                    y={y}
-                    className="fill-muted-foreground text-xs"
-                    textAnchor={x > cx ? 'start' : 'end'}
-                    dominantBaseline="central"
-                  >
-                    {data[index].product.length > 12
-                      ? data[index].product.substring(0, 12).concat('...')
-                      : data[index].product}{' '}
-                    ({value})
-                  </text>
-                )
-              }}
-            >
-              {data.map((_, idx) => {
-                return (
-                  <Cell
-                    key={`cell-${idx}`}
-                    fill={COLORS[idx]}
-                    className="stroke-card hover:opacity-80 "
-                  />
-                )
-              })}
-            </Pie>
-          </PieChart>
-        </ResponsiveContainer>
+                  return (
+                    <text
+                      x={x}
+                      y={y}
+                      className="fill-muted-foreground text-xs"
+                      textAnchor={x > cx ? 'start' : 'end'}
+                      dominantBaseline="central"
+                    >
+                      {popuularProdutcs[index].product.length > 12
+                        ? popuularProdutcs[index].product
+                            .substring(0, 12)
+                            .concat('...')
+                        : popuularProdutcs[index].product}{' '}
+                      ({value})
+                    </text>
+                  )
+                }}
+              >
+                {popuularProdutcs.map((_, idx) => {
+                  return (
+                    <Cell
+                      key={`cell-${idx}`}
+                      fill={COLORS[idx]}
+                      className="stroke-card hover:opacity-80 "
+                    />
+                  )
+                })}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+        )}
       </CardContent>
     </Card>
   )
